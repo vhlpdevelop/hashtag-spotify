4154c02f80703999507fa192a8400586b7453d4d
damorimsantos <diego@hashtagtreinamentos.com>
2025-02-08 21:54:18 -0300

    finalizando a aula complementar de css com ajustes de jsx

--- src/pages/Home/SingleItem.jsx
+++ src/pages/Home/SingleItem.jsx
@@ -6,14 +6,16 @@ import { faCirclePlay } from "@fortawesome/free-solid-svg-icons";
 const SingleItem = ({ path, image, name, type, _id, artist, style }) => {
   return (
     <Link to={`${path}/${_id}`} className="single-item">
-      <div className="single-item__div-image">
-        <img
-          src={image}
-          alt={`Imagem de ${name}`}
-          className={`single-item__image ${
-            style === "square" ? "single-item__image--square" : ""
-          }`}
-        />
+      <div className="single-item__div-image-button">
+        <div className="single-item__div-image">
+          <img
+            src={image}
+            alt={`Imagem de ${name}`}
+            className={`single-item__image ${
+              style === "square" ? "single-item__image--square" : ""
+            }`}
+          />
+        </div>
 
         <FontAwesomeIcon className="single-item__icon " icon={faCirclePlay} />
       </div>
